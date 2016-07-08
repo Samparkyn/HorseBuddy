0e2bebb5712e82e2c13a387ffb7191cf4ad53c63
Samparkyn <samparkyn101@gmail.com>
2016-07-08 13:45:01 +0100

    add competition controller and entity

---
+++ src/AppBundle/Controller/CompetitionController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Competition;
+
+class CompetitionController extends Controller
+{
+  
+}
--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -3,9 +3,15 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class MainController extends Controller
 {
+  /**
+  * @Route("/", name="home")
+  * @Method({"GET"})
+  */
     public function homepageAction()
     {
         return $this->render('main/homepage.html.twig');
