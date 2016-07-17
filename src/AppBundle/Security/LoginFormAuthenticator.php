3683924959358417bc5cf367bb5548e148d570e4
Samparkyn <samparkyn101@gmail.com>
2016-07-17 20:42:59 +0100

    add authenticator for login form

---
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Security;
+
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+  public function getCredentials(Request $request)
+    {
+      $isLoginSubmit = $request->getPathInfo() == '/login' && $request->isMethod('POST');
+      if (!$isLoginSubmit) {
+           // skip authentication
+           return;
+       }
+       
+        $form = $this->formFactory->create(LoginForm::class);
+        $form->handleRequest($request);
+        
+        $data = $form->getData();
+        return $data;
+    }
+    
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+      
+    }
+    
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+      
+    }
+    
+    protected function getLoginUrl()
+   {
+     
+   }
+   
+   protected function getDefaultSuccessRedirectUrl()
+   {
+     
+   }
+   
+   private $formFactory;
+   
+   public function __construct(FormFactoryInterface $formFactory)
+   {
+     $this->formFactory = $formFactory;
+   }
+}
